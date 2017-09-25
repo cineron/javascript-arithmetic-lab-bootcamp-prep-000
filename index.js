082c9288f4be31c78651be7e5bb908cd96b765bb
Ron Wilson <cineron+flatiron@gmail.com>
2017-09-25 04:06:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    var add = a + b;
+    return add;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
